60fc4ac9d8155cd806a3339e970b51b4ebeb59dd
vinayak.chaturvedi96@gmail.com <vinayak.chaturvedi96@gmail.com>
2021-03-08 19:00:56 +0530

    Handling Test cases.

--- src/test/java/ScientificCalculatorTest.java
+++ src/test/java/ScientificCalculatorTest.java
@@ -29,7 +29,7 @@ public class ScientificCalculatorTest {
         InputStream in = new ByteArrayInputStream("4".getBytes());
         System.setIn(in);
         ScientificCalculator.handleSquareRoot(new Scanner(System.in));
-        Assert.assertEquals("Enter the number: Square root of 4 is: 2.0\n", outContent.toString());
+        Assert.assertEquals("Enter the number: Square root of 4.0 is: 2.0\n", outContent.toString());
     }
 
     @Test
@@ -45,7 +45,7 @@ public class ScientificCalculatorTest {
         InputStream in = new ByteArrayInputStream("4".getBytes());
         System.setIn(in);
         ScientificCalculator.handleLogarithm(new Scanner(System.in));
-        Assert.assertEquals("Enter the number: Natural Logarithmic value of 4 is: 1.3862943611198906\n", outContent.toString());
+        Assert.assertEquals("Enter the number: Natural Logarithmic value of 4.0 is: 1.3862943611198906\n", outContent.toString());
     }
 
     @Test
@@ -53,7 +53,7 @@ public class ScientificCalculatorTest {
         InputStream in = new ByteArrayInputStream("4\n2".getBytes());
         System.setIn(in);
         ScientificCalculator.handlePower(new Scanner(System.in));
-        Assert.assertEquals("Enter the base number: Enter the power: Power function: 4^2 = 16.0\n", outContent.toString());
+        Assert.assertEquals("Enter the base number: Enter the power: Power function: 4.0^2.0 = 16.0\n", outContent.toString());
     }
 
     @Test
@@ -61,7 +61,7 @@ public class ScientificCalculatorTest {
         InputStream in = new ByteArrayInputStream("5".getBytes());
         System.setIn(in);
         ScientificCalculator.handleSquareRoot(new Scanner(System.in));
-        Assert.assertNotEquals("Enter the number: Square root of 4 is: 2.0\n", outContent.toString());
+        Assert.assertNotEquals("Enter the number: Square root of 4.0 is: 2.0\n", outContent.toString());
     }
 
     @Test
@@ -77,7 +77,7 @@ public class ScientificCalculatorTest {
         InputStream in = new ByteArrayInputStream("5".getBytes());
         System.setIn(in);
         ScientificCalculator.handleLogarithm(new Scanner(System.in));
-        Assert.assertNotEquals("Enter the number: Natural Logarithmic value of 4 is: 1.3862943611198906\n", outContent.toString());
+        Assert.assertNotEquals("Enter the number: Natural Logarithmic value of 4.0 is: 1.3862943611198906\n", outContent.toString());
     }
 
     @Test
@@ -85,6 +85,6 @@ public class ScientificCalculatorTest {
         InputStream in = new ByteArrayInputStream("4\n4".getBytes());
         System.setIn(in);
         ScientificCalculator.handlePower(new Scanner(System.in));
-        Assert.assertNotEquals("Enter the base number: Enter the power: Power function: 4^4 = 16.0\n", outContent.toString());
+        Assert.assertNotEquals("Enter the base number: Enter the power: Power function: 4.0^4.0 = 16.0\n", outContent.toString());
     }
 }
